dc86567328c8c3bb260fac79ea2b081531c7df8f
FurqatMashrabjonov <php_lesson@mail.ru>
2023-02-08 23:08:19 +0500

    das

--- inline_keyboards.php
+++ inline_keyboards.php
@@ -20,19 +20,19 @@ return [
     ],
     'examples' => [
 
-        [['text' => 'Объёмные буквы', 'url' => 'https://telegra.ph/KATALOG-09-17']],
-        [['text' => 'Световой короб', 'url' => 'https://telegra.ph/KATALOG-09-17-2']],
-        [['text' => 'Реклама на авто', 'url' => 'https://telegra.ph/KATALOG-09-17-3']],
-        [['text' => 'Таблички', 'url' => 'https://telegra.ph/KATALOG-09-17-4']],
+        [['text' => 'Объёмные буквы', 'url' => 'https://telegra.ph/KATALOG-09-17'],
+        ['text' => 'Световой короб', 'url' => 'https://telegra.ph/KATALOG-09-17-2']],
+        [['text' => 'Реклама на авто', 'url' => 'https://telegra.ph/KATALOG-09-17-3'],
+        ['text' => 'Таблички', 'url' => 'https://telegra.ph/KATALOG-09-17-4']],
         [['text' => 'Оформления', 'url' => 'https://telegra.ph/KATALOG-09-17-5']],
-        [['text' => 'Туманка', 'url' => 'https://telegra.ph/KATALOG-09-17-6']],
-        [['text' => 'Керамические буквы', 'url' => 'https://telegra.ph/KATALOG-09-17-8']],
-        [['text' => 'Керамический герб', 'url' => 'https://telegra.ph/KATALOG-09-17-8']],
-        [['text' => 'Стеллы', 'url' => 'https://telegra.ph/KATALOG-09-17-9']],
-        [['text' => 'Неон', 'url' => 'https://telegra.ph/KATALOG-09-17-10']],
-        [['text' => 'Видео буквы', 'url' => 'https://telegra.ph/KATALOG-09-17-11']],
-        [['text' => 'Картина из Холста', 'url' => 'https://telegra.ph/KATALOG-09-17-12']],
-        [['text' => 'Крышевая конструкция', 'url' => 'https://telegra.ph/KATALOG-09-17-13']]
+        ['text' => 'Туманка', 'url' => 'https://telegra.ph/KATALOG-09-17-6'],
+        [['text' => 'Керамические буквы', 'url' => 'https://telegra.ph/KATALOG-09-17-8'],
+        ['text' => 'Керамический герб', 'url' => 'https://telegra.ph/KATALOG-09-17-8']],
+        [['text' => 'Стеллы', 'url' => 'https://telegra.ph/KATALOG-09-17-9'],
+        ['text' => 'Неон', 'url' => 'https://telegra.ph/KATALOG-09-17-10']],
+        [['text' => 'Видео буквы', 'url' => 'https://telegra.ph/KATALOG-09-17-11'],
+        ['text' => 'Картина из Холста', 'url' => 'https://telegra.ph/KATALOG-09-17-12'],
+        ['text' => 'Крышевая конструкция', 'url' => 'https://telegra.ph/KATALOG-09-17-13']]
 
     ]
 ];
